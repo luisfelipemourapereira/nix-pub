ef4f11904114d16b18f94c94e665b4233c1b66d5
Luis De Siqueira <luis@pleme.io>
2023-01-13 13:32:56 -0300

    flake lock

--- flake.nix
+++ flake.nix
@@ -1,17 +1,9 @@
 {
   description = "my own public nix things";
-  inputs.flake-utils.url = github:numtide/flake-utils;
-  outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        system = "x86_64-linux";
-        localPkgs = import ./packages/top-level.nix;
-        localModules = import ./modules/top-level.nix;
-      in
-      rec {
-        packages = flake-utils.lib.flattenTree localPkgs;
-        nixosModules.default = localModules.blackmatter;
-      }
-    );
+  # inputs.flake-utils.url = github:numtide/flake-utils;
+  outputs = { self }:
+    let
+      localModules = import ./modules/top-level.nix;
+    in
+    { nixosModules = localModules; };
 }
