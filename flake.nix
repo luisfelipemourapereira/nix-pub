c987f208b9f467a10294c0edec8b454151975262
Luis De Siqueira <luis@pleme.io>
2023-01-13 14:42:26 -0300

    flake lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "my own public nix things";
-  # inputs.flake-utils.url = github:numtide/flake-utils;
   outputs = { self }:
     let
       localModules = import ./modules/top-level.nix;
