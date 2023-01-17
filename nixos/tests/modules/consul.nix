71a95bc28c41631c1952a9c3e7c272d89e0e7b2d
luis <luis>
2023-01-17 13:58:30 -0800

    testing

---
+++ nixos/tests/modules/consul.nix
@@ -0,0 +1,25 @@
+################################################################################
+# test: ./modules/services/consul.nix
+################################################################################
+let
+  mkTest = import ../../../../functions/testing/mkTest.nix;
+  case_0 = {
+    hostName = "consul";
+  };
+in
+mkTest {
+  name = "consul";
+  testScript = ''
+    start_all()
+    machine = ${case_0.hostName}
+
+    machine.wait_for_unit("network-online.target")
+    assert "${case_0.hostName}" == machine.succeed("hostname --fqdn").strip()
+  '';
+
+  nodes.machine =
+    {
+      imports = [ ../../../../modules/services/consul.nix ];
+      consul.enable = true;
+    };
+}
